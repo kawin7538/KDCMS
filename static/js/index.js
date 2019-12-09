e576b5543b337e18ccf55d2a557c8cf4e29b046a
Kawin Chinpong <kawin7538@hotmail.com>
2019-12-09 14:14:33 +0700

    Rearrange File structure

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
+    var $videoSrc;
     $('button[id^=yt_button]').on('click', function (e) {
         // do something...
         var value=$(this).val();
-        console.log(value);
-        $("#yt_video").html('<iframe src="https://www.youtube.com/embed/'+value+'?autoplay=1" width="100%" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+        $videoSrc="https://www.youtube.com/embed/'"+value+"'?autoplay=1"
+        console.log($videoSrc);
+        $("#yt_frame").html('<iframe id="yt_video" class="embed-responsive-item" src="https://www.youtube.com/embed/'+value+'?autoplay=1" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
     });
+    $('#videoModal').on('hide.bs.modal', function (e) {
+        // a poor man's stop video
+        $("#yt_video").attr('src',$videoSrc); 
+    }) 
 });
