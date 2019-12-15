282feb6d543ddc95174e9a2b488309b64dd25c95
Kawin Chinpong <kawin7538@hotmail.com>
2019-12-16 03:30:31 +0700

    (almost) Complete evaluation report and form

--- evaluation/views.py
+++ evaluation/views.py
@@ -9,19 +9,21 @@ from django.http import JsonResponse
 
 from dbHelper import firestore,db
 
+import collections, functools, operator 
+
 # Create your views here.
 def index(request,pk1,pk2):
     data={
         'pk1':pk1,
         'pk2':pk2,
     }
-    event_ref=db.collection('event').document(pk1)
-    event_data=event_ref.get().to_dict()
-    data['event_name']=event_data['event_name']
-    event_data['line_item']=event_data['line_item'][pk2]
-    song_ref=db.collection('song').where('song_id','==',event_data['line_item']['song_id'])
-    for doc in song_ref.stream():
-        data['song_name']=(doc.to_dict())['song_name']
+    # event_ref=db.collection('event').document(pk1)
+    # event_data=event_ref.get().to_dict()
+    # data['event_name']=event_data['event_name']
+    # event_data['line_item']=event_data['line_item'][pk2]
+    # song_ref=db.collection('song').where('song_id','==',event_data['line_item']['song_id'])
+    # for doc in song_ref.stream():
+    #     data['song_name']=(doc.to_dict())['song_name']
     # member_ref=db.collection('member').where('member_id','in',event_data['line_item']['member'])
     # print(event_data)
     # for doc in member_ref.stream():
@@ -39,15 +41,33 @@ class evaluation_list(View):
         song_ref=db.collection('song').where('song_id','==',event_data['line_item']['song_id'])
         for doc in song_ref.stream():
             data['song_name']=(doc.to_dict())['song_name']
-
-        eva_ref=db.collection('evaluation').where('event_id','==',int(pk1)).where('team_id','==',int(pk2))
+        #Score From Criteria and Score Overall
+        eva_ref=db.collection('evaluate').where('event_id','==',int(pk1)).where('team_id','==',int(pk2))
+        criteria_ref=db.collection('criteria')
         data['list_evaluation']=[]
+        temp_list_score=[]
         for doc in eva_ref.stream():
             temp_dict=dict()
-            temp_dict['eva_date']=doc['eva_date']
-            temp_dict['mean_score']=doc['mean_score']
-
+            temp_doc=doc.to_dict()
+            temp_dict['eva_id']=temp_doc['eva_id']
+            temp_dict['eva_date']=temp_doc['eva_date']
+            temp_dict['mean_score']=temp_doc['mean_score']
+            temp_dict['criteria_score']=temp_doc['score_lineitem']
+            temp_list_score.append(temp_doc['member_lineitem'])
+            for cri_doc in criteria_ref.stream():
+                temp=cri_doc.to_dict()
+                # print(temp['criteria_id'],temp['criteria_name'])
+                # print(temp_dict['criteria_score'][str(temp['criteria_id'])])
+                # print(temp_dict['criteria_score'])
+                temp_dict['criteria_score'][str(temp['criteria_id'])]=[temp_dict['criteria_score'][str(temp['criteria_id'])],temp['criteria_name']]
             data['list_evaluation'].append(temp_dict)
-
+        data['list_evaluation'].sort(key=lambda i:i['eva_date'])
+        #member Part
+        member_result = dict(functools.reduce(operator.add, map(collections.Counter, temp_list_score)))
+        member_ref=db.collection('member').where('member_id','in',event_data['line_item']['member'])
+        for doc in member_ref.stream():
+            temp_doc=doc.to_dict()
+            member_result[str(temp_doc['member_id'])]=[member_result[str(temp_doc['member_id'])],temp_doc['nickname']]
+        data['list_member']=member_result
         print(data)
         return JsonResponse(data)
--- static/js/evaluation.js
+++ static/js/evaluation.js
@@ -2,13 +2,58 @@ $(document).ready(function(){
     list_rehearsal();
 });
 
+
+
 function list_rehearsal(){
     $.ajax({
         url: '../../evaluation/list/'+$("#pk1").html()+"/"+$("#pk2").html(),
         type: 'get',
         dataType: 'json',
+        beforeSend: function(){
+            $("#loading").modal('show');
+        },
         success: function(data){
-            console.log(data)
+            $("#event_name").html(data.event_name);
+            $("#song_name").html(data.song_name);
+            let $row = ``;
+            let $row2 = ``;
+            $.each(data.list_member,function(k,v){
+                $row += `<tr>`;
+                $row += `<td>${v[1]}</td>`;
+                $row += `<td>${v[0]}</td>`;
+                $row += `</tr>`;
+                $row2 += `<tr>`;
+                $row2 += `<td><label for="check_in${k}">${v[1]}</label></td>`;
+                $row2 += `<td><input type="checkbox" id="check_in${k}" name="check_in" value="${k}"></td>`;
+                $row2 += `</tr>`;
+            });
+            $("#member_checkin > tbody").html($row);
+            $("#member_check_table > tbody").html($row2);
+            $row= ``;
+            data.list_evaluation.forEach(s => {
+                $row += `<tr>`;
+                $row += `<td rowspan=${Object.keys(s.criteria_score).length} id="eva_id" style="display:none">${s.eva_id}</td>`
+                $row += `<td rowspan=${Object.keys(s.criteria_score).length}>${s.eva_date}</td>`;
+                // row += `<td rowspan=${s.criteria.length}>${s.eva_date}</td>`;
+                var ii=0;
+                $.each(s.criteria_score,function(k,v){
+                    if(ii==0){
+                        ii++;
+                    }
+                    else{
+                        $row += `<tr>`;
+                    }
+                    $row += `<td>${v[1]}</td>`;
+                    $row += `<td>${v[0]}</td>`;
+                    $row += `</tr>`;
+                })
+            });
+            // console.log($row);
+            $("#eva_score > tbody").html($row);
+        },
+        complete: function(){
+            $("#loading").modal("hide");
         }
     })
 }
+
