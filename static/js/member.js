c5a367d4cf1f9127aa969ca95e8d64aaf238abf0
satjasuda <58803676+satjasuda@users.noreply.github.com>
2019-12-15 17:16:03 +0700

    Save member session

--- static/js/member.js
+++ static/js/member.js
@@ -25,3 +25,79 @@ function listMember(){
         }
     });
 }
+
+function saveData() {
+    var nickname= document.getElementById('nickname');
+    var firstname= document.getElementById('firstname');
+    var lastname= document.getElementById('lastname');
+    var studentid= document.getElementById('studentid');
+    var faculty;
+    var image;
+    var tel= document.getElementById('phonenumber');
+    var facebook= document.getElementById('facebook');
+    var line= document.getElementById('line');
+    var email= document.getElementById('email');
+    insertData(nickname.value,firstname.value,lastname.value,studentid.value,
+        phonenumber.value,facebook.value,line.value,email.value)
+
+}
+
+function insertData(nickname,firstname,lastname,studentid,
+    phonenumber,facebook,line,email)
+    var firebaseref=firebase.database().ref("users");
+    firebaseRef.push({
+        nickname:nickname,
+        firstname:firstname,
+        lastname:lastname,
+        studentid=studentid,
+        phonenumber=phonenumber,
+        facebook=facebook,
+        line=line,
+        email=email,
+    });
+    console.log("Insert Success");
+
+session_start();
+mysql_connect("localhost","root","root");
+mysql_select_db("mydatabase");
+
+
+$(document).on('click','memberFormReset',function(){
+    $("#memberForm input[id!=member_id]").val('');
+});
+
+$(document).on('click',"#memberFormSubmit",function(e){
+    var nickname = $('#nickname').val().trim();
+    if (nickname == '') {
+        alert('กรุณาระบุ nickname');
+        $('#nickname').focus();
+        return false;
+        }
+
+    var firstname = $('#firstname').val().trim();
+        if (firstname == '') {
+            alert('กรุณาระบุ firstname');
+            $('#firstname').focus();
+            return false;
+            }
+    var lastname = $('#lastname').val().trim();
+        if (lastname == '') {
+            alert('กรุณาระบุ lastname');
+            $('#lastname').focus();
+            return false;
+            }
+    var studentid = $('#studentid').val().trim();
+        if (studentid == '') {
+            alert('กรุณาระบุ studentID');
+            $('#studentid').focus();
+            return false;
+            }
+
+$(document).on('click',"#upload",function(){
+$("#member_id").val("<new>");
+$("#memberForm input[id!=member_id]").val('');
+});
+            
+
+    })
+    
